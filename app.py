2bfe0ae3178db278a720c1dc0683fec7339c565c
niciyan <nishiyamasu@gmail.com>
2017-11-18 10:43:11 +0900

    added tag feature

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import datetime
 from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import Required, Length
 from flask_mongoengine import MongoEngine
-from flask_mongoengine.wtf import model_form
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -15,18 +14,27 @@ app.config["MONGODB_SETTINGS"] = { "db":"nishi" }
 app.config['SECRET_KEY'] = "ok"
 app.debug = True
 
+class Memo(db.EmbeddedDocument):
+    tag = db.StringField(max_length=20)
+    body = db.StringField(max_length=150)
+
 class Post(db.Document):
     title = db.StringField(max_length=100, required=True)
     content = db.StringField(max_length=200, required=True)
-    tags = db.ListField(db.StringField(max_length=10), mix_entries=1)
+    memo = db.EmbeddedDocumentField(Memo)
     created_at = db.DateTimeField(default=datetime.datetime.now)
 
 # PostForm = model_form(Post)
 
 class PostForm(FlaskForm):
     title = StringField('Title',validators=[Required(), Length(1,80)])
-    content = StringField('Content',validators=[Required(), Length(1,180)])
-    submit = SubmitField('submit')
+    content = TextAreaField('Content',validators=[Required(), Length(1,180)])
+    submit = SubmitField('Submit')
+
+class MemoForm(FlaskForm):
+    tag = StringField("Tag", validators=[Length(1,20)])
+    body = TextAreaField("Memo", validators=[Length(1,150)])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
@@ -53,6 +61,31 @@ def delete():
     flash("the post deleted")
     return redirect(url_for("index"))
 
+@app.route('/edit-memo/<string:_id>', methods=['GET', 'POST'])
+def edit_memo(_id):
+    form = MemoForm()
+    post = Post.objects.get(id=_id)
+    if form.validate_on_submit():
+        post.memo.tag = form.tag.data
+        post.memo.body = form.body.data
+        post.save()
+        return redirect(url_for("index"))
+    form.tag.data = post.memo.tag
+    form.body.data = post.memo.body 
+    return render_template('edit_memo.html', form=form, post=post)
+
+@app.route('/create-memo/<string:_id>', methods=['GET', 'POST'])
+def create_memo(_id):
+    form = MemoForm()
+    post = Post.objects.get(id=_id)
+    if form.validate_on_submit():
+        new_memo = Memo()
+        new_memo.tag = form.tag.data
+        new_memo.body = form.body.data
+        post.memo = new_memo
+        post.save()
+        return redirect(url_for("index"))
+    return render_template('edit_memo.html', form=form, post=post)
 
 
 if __name__ == '__main__':
